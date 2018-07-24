d066bd8d1f6e5854b1fb2c6a10b049ef066eb4a1
sdickreuter <Simon.Dickreuter@uni-tuebingen.de>
2018-07-24 12:21:27 +0200

    update

--- simulate_dipole.py
+++ simulate_dipole.py
@@ -48,9 +48,14 @@ def make_dipole(theta,alpha_max,num):
     t0s = np.zeros((num))
     return Wavelets(r=rs, k=ks, t0=t0s, wavelength=0.03, phases=phases, mode=modes['ray'])
 
+
 plotit = True
 
-num = 100
+iterations = 500
+
+theta = np.pi/2
+
+num = 201
 
 lense1 = Lense(x=0.0, y=0,r1=np.inf,r2=2.0,height=0.5, num=num)
 lense2 = Lense(x=0.0, y=0,r1=-2.0,r2=np.inf,height=0.5, num=num)
@@ -62,10 +67,41 @@ lense2.shift(dx=lense1.back.points[:,0].max()+lense1._calc_f_back()+lense2._calc
 #lense1.shift(dx=lense1.f)
 #lense2.shift(dx=lense1.back.points[:,0].max()+lense1._calc_f_back()*2)
 
-
-theta = 0#np.pi/2
 alpha_max = np.real(angle_between(np.array([1,0]),lense1.front.points[0,:]))#np.pi/10)
 print("alpha_max: "+str(alpha_max)+'  '+str(alpha_max*180/np.pi))
+
+
+dipole = make_dipole(theta, np.pi/2,100000)
+angles = np.zeros(dipole.k.shape[0])
+for i in range(len(angles)):
+    if dipole.k[i,1] < 0:
+        angles[i] = -angle_between(dipole.k[i,:],np.array([1,0])).real
+    else:
+        angles[i] = angle_between(dipole.k[i,:],np.array([1,0])).real
+
+plt.hist(angles,bins=100)
+plt.axvline(x=alpha_max,color='k',linestyle='--')
+plt.axvline(x=-alpha_max,color='k',linestyle='--')
+plt.xlabel("angle to optical axis / rad")
+plt.ylabel("occurance")
+plt.savefig("dipole_"+str(int(np.round(theta*180/np.pi)))+"_theta_source_histogramm.png", dpi=600)
+plt.show()
+
+dipole = make_dipole(theta, alpha_max,100000)
+angles = np.zeros(dipole.k.shape[0])
+for i in range(len(angles)):
+    if dipole.k[i,1] < 0:
+        angles[i] = -angle_between(dipole.k[i,:],np.array([1,0])).real
+    else:
+        angles[i] = angle_between(dipole.k[i,:],np.array([1,0])).real
+
+plt.hist(angles,bins=100)
+plt.xlabel("angle to optical axis / rad")
+plt.ylabel("occurance")
+plt.savefig("dipole_"+str(int(np.round(theta*180/np.pi)))+"_theta_source_histogramm_alphamax.png", dpi=600)
+plt.show()
+
+
 # num = 10000
 # ks = np.zeros((num, 2))
 # ks[:, 0] = np.repeat(1.0, num)
@@ -103,7 +139,7 @@ print("alpha_max: "+str(alpha_max)+'  '+str(alpha_max*180/np.pi))
 # plt.show()
 
 
-num = 300
+num = 200
 ys = np.linspace(-0.5, 0.5, num)
 #xs = np.repeat(lense2.x+lense2._calc_f_back(), num)
 #xs = np.repeat(lense2.back.points[:,0].max()+lense2._calc_f_back(), num)
@@ -112,7 +148,7 @@ xs = np.repeat(15.92, num)
 screen = Surface(np.vstack((xs, ys)).T, reflectivity=0.0, transmittance=1.0, n1=1.0, n2=1.0)
 screen.flip_normals()
 
-num = 1000
+num = 100
 
 dipole = make_dipole(theta, alpha_max,num)
 print("dipole: " + str(dipole.n))
@@ -145,34 +181,34 @@ if plotit:
     divider = 50#200
     plt.plot(dipole.r[:, 0], dipole.r[:, 1])
     for i in range(dipole.r.shape[0]):
-        plt.plot(dipole.r[i, 0], dipole.r[i, 1], "bo")
+        #plt.plot(dipole.r[i, 0], dipole.r[i, 1], "bo")
         plt.arrow(dipole.r[i, 0], dipole.r[i, 1], dipole.k[i, 0] / (divider*10), dipole.k[i, 1] / (divider*10))
     plt.plot(lense1.front.points[:, 0], lense1.front.points[:, 1])
     for i in range(onlense1_front.r.shape[0]):
-        plt.plot(onlense1_front.r[i, 0], onlense1_front.r[i, 1], "bo")
+        #plt.plot(onlense1_front.r[i, 0], onlense1_front.r[i, 1], "bo")
         plt.arrow(onlense1_front.r[i, 0], onlense1_front.r[i, 1], onlense1_front.k[i, 0] / (divider*10), onlense1_front.k[i, 1] / (divider*10))
     plt.plot(lense1.back.points[:, 0], lense1.back.points[:, 1])
     for i in range(onlense1_back.r.shape[0]):
-        plt.plot(onlense1_back.r[i, 0], onlense1_back.r[i, 1], "bo")
+        #plt.plot(onlense1_back.r[i, 0], onlense1_back.r[i, 1], "bo")
         plt.arrow(onlense1_back.r[i, 0], onlense1_back.r[i, 1], onlense1_back.k[i, 0] / divider, onlense1_back.k[i, 1] / divider)
     plt.plot(lense2.front.points[:, 0], lense2.front.points[:, 1])
     for i in range(onlense2_front.r.shape[0]):
-        plt.plot(onlense2_front.r[i, 0], onlense2_front.r[i, 1], "bo")
+        #plt.plot(onlense2_front.r[i, 0], onlense2_front.r[i, 1], "bo")
         plt.arrow(onlense2_front.r[i, 0], onlense2_front.r[i, 1], onlense2_front.k[i, 0] / (divider*10),
                   onlense2_front.k[i, 1] / (divider*10))
     plt.plot(lense2.back.points[:, 0], lense2.back.points[:, 1])
     for i in range(onlense2_back.r.shape[0]):
-        plt.plot(onlense2_back.r[i, 0], onlense2_back.r[i, 1], "bo")
+        #plt.plot(onlense2_back.r[i, 0], onlense2_back.r[i, 1], "bo")
         plt.arrow(onlense2_back.r[i, 0], onlense2_back.r[i, 1], onlense2_back.k[i, 0] / divider,
                   onlense2_back.k[i, 1] / divider)
     plt.plot(screen.points[:, 0], screen.points[:, 1])
     # for i in range(onscreen.r.shape[0]):
     #     plt.plot(onscreen.r[i, 0], onscreen.r[i, 1], "bo")
     #     plt.arrow(onscreen.r[i, 0], onscreen.r[i, 1], onscreen.k[i, 0] / divider, onscreen.k[i, 1] / divider)
+    plt.savefig("dipole_" + str(int(np.round(theta * 180 / np.pi))) + "_theta_setup.svg", dpi=600)
     plt.show()
 
 
-iterations = 500
 
 prog = progress.Progress(max=iterations)
 num=500
@@ -201,14 +237,15 @@ for i in range(iterations):
 
 
 plt.plot(screen.midpoints[:,1],screen.field ** 2)
-#plt.plot(screen.midpoints[:,1],screen.field[:,1] ** 2)
-plt.savefig("dipole.png", dpi=600)
+plt.xlabel("position on screen / m")
+plt.ylabel("intensity / a.u.")
+plt.savefig("dipole_"+str(int(np.round(theta*180/np.pi)))+"_theta_onscreen.png", dpi=600)
 plt.show()
 
 plt.plot(screen.midpoints[:,1],screen.hits)
+plt.xlabel("position on screen / m")
+plt.ylabel("number of hits")
+plt.savefig("dipole_"+str(int(np.round(theta*180/np.pi)))+"_theta_onscreen_hits.png", dpi=600)
 plt.show()
 
 
-# plt.plot(screen2.midpoints[:,0],screen2.hits)
-# plt.savefig("wavelet_lense_screenx.png", dpi=600)
-# plt.show()
